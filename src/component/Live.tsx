38a5948eb8a26dd9949d40a09cf6cfe37e0aac3e
nosuke222219 <57469327+nosuke222219@users.noreply.github.com>
2022-02-04 14:13:36 +0900

    fix_live_size

--- src/component/Live.tsx
+++ src/component/Live.tsx
@@ -13,9 +13,20 @@ const Live: React.FC = () => {
       <Logo className={style.logo} />
       <Title className={style.title} />
       <div className={style.cover}></div>
-      <Hamburger className={style.hamburger} checked={checked} setChecked={setChecked}/>
-      <Menu className={`${style.menu} ${checked ? style.menuShow : ""}`}/>
-      <iframe className={style.iframe} src="https://www.youtube.com/embed/GVaLs79zT1k?autoplay=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <Hamburger
+        className={style.hamburger}
+        checked={checked}
+        setChecked={setChecked}
+      />
+      <Menu className={`${style.menu} ${checked ? style.menuShow : ""}`} />
+      <iframe
+        className={style.iframe}
+        src="https://www.youtube.com/embed/GVaLs79zT1k"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
