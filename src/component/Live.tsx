814f2044096d36f6da7b74f2d50dbfebf70499cd
nosuke222219 <57469327+nosuke222219@users.noreply.github.com>
2023-10-18 18:08:14 +0900

    live bg set

--- src/component/Live.tsx
+++ src/component/Live.tsx
@@ -19,14 +19,9 @@ const Live: React.FC = () => {
         setChecked={setChecked}
       />
       <Menu className={`${style.menu} ${checked ? style.menuShow : ""}`} />
-      <iframe
-        className={style.iframe}
-        src="https://www.youtube.com/embed/GVaLs79zT1k"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <div className={style.livebg}>
+        <h1>生放送は終了しました。</h1>
+      </div>
     </div>
   );
 };
